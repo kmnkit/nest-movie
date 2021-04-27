594aad530f1c0e79332fd54a101dd5d1e6afa8ea
Marco Kang <kmnkit@gmail.com>
2021-04-27 23:57:57 +0900

    nest api end

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Patch,
+  Body,
+  Query,
+} from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie with a title: ${searchingYear}`;
+  }
+
+  @Get(':id')
+  getOne(@Param('id') movieId: number) {
+    return this.moviesService.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() movieData: CreateMovieDto) {
+    return this.moviesService.create(movieData);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') movieId: number) {
+    return this.moviesService.deleteOne(movieId);
+  }
+
+  @Patch(':id')
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
+    return this.moviesService.update(movieId, updateData);
+  }
+}
